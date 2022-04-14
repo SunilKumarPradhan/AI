63a1ae16b45c98a513ca92882b617e49e2e67d65
sunilkumarpradhan <89890497+SunilKumarPradhan@users.noreply.github.com>
2022-04-14 15:43:31 +0530

    Update and rename amadeus.py to AI Chatbot.py

--- AI Chatbot.py
+++ AI Chatbot.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "Your OpenAI key"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "Your Telegram Key"
+    tgkey = "5356128034:AAFyF7Rtfs0AoQpqr_BorprvWcSFFqKmlg8"
 else:
     tgkey = tgenv
 print(tgenv)
